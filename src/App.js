cc1ba597dfb169efe1221f6ad2581294f18464ed
pccgaucho <pccgaucho@gmail.com>
2023-05-06 20:53:01 -0300

    Teste de carregamento React JS

--- src/App.js
+++ src/App.js
@@ -3,20 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Aula 01 de React JS</h1>
+
+        <button>Click me</button>
       </header>
     </div>
   );
